ba024106b1ce1c3387bcbf6b8ca4e950a587de62
SangHun <IT.sanghun.yoo@gmail.com>
2025-01-23 11:25:34 +0900

    Chart Example 5 Completed

---
+++ Chart_Example/View/ChartExample_5_Form.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Chart_Example.View
+{
+    partial class ChartExample_5_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chartControl = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chartControl
+            // 
+            chartArea1.AxisX.IsLabelAutoFit = false;
+            chartArea1.AxisX.LabelStyle.Font = new System.Drawing.Font("맑은 고딕", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            chartArea1.AxisY.IsLabelAutoFit = false;
+            chartArea1.AxisY.LabelStyle.Font = new System.Drawing.Font("맑은 고딕", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            chartArea1.Name = "ChartArea1";
+            this.chartControl.ChartAreas.Add(chartArea1);
+            this.chartControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.Name = "Legend1";
+            this.chartControl.Legends.Add(legend1);
+            this.chartControl.Location = new System.Drawing.Point(0, 0);
+            this.chartControl.Name = "chartControl";
+            series1.ChartArea = "ChartArea1";
+            series1.Font = new System.Drawing.Font("맑은 고딕", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chartControl.Series.Add(series1);
+            this.chartControl.Size = new System.Drawing.Size(800, 450);
+            this.chartControl.TabIndex = 2;
+            this.chartControl.Text = "chart1";
+            // 
+            // ChartExample_5_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chartControl);
+            this.Name = "ChartExample_5_Form";
+            this.Text = "ChartExample_5_Form";
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartControl;
+    }
+}
---
+++ Chart_Example/View/ChartExample_5_Form.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Chart_Example.View
+{
+    public partial class ChartExample_5_Form : TemplateForm
+    {
+        private DataTable dataSource;
+
+        public ChartExample_5_Form()
+        {
+            InitializeComponent();
+
+            this.SetData();
+            this.SetChart();
+        }
+
+        private void SetData()
+        {
+            this.dataSource = new DataTable("ChartData");
+
+            string[] subjects = new string[] { "Excel", "Access", "Power Point", "Word", "Visio" };
+            int[,] values = new int[,] {
+                                    {9, 4, 5},
+                                    {8, 8, 5},
+                                    {5, 10, 7},
+                                    {5, 10, 6},
+                                    {4, 5, 10}
+                                };
+
+            this.dataSource.Columns.Add("실무");
+            this.dataSource.Columns.Add("오일남");
+            this.dataSource.Columns.Add("홍길동");
+            this.dataSource.Columns.Add("김덕수");
+
+            for (int i = 0; i < subjects.Length; i++)
+            {
+                DataRow row = this.dataSource.NewRow();
+
+                row[0] = subjects[i];
+
+                for (int j = 0; j < values.GetLength(1); j++)
+                {
+                    row[j + 1] = values[i, j];
+                }
+
+                this.dataSource.Rows.Add(row);
+            }
+        }
+
+        private void SetChart()
+        {
+            this.chartControl.DataSource = this.dataSource;
+
+            this.chartControl.Titles.Add(
+                new Title("직원의 사무 기술 능력 점수", Docking.Top,
+                            new Font("Malgun Gothic", 20, FontStyle.Bold), Color.Black)
+                );
+
+            this.chartControl.Series.RemoveAt(0);
+
+            for (int i = 1; i < this.dataSource.Columns.Count; i++)
+            {
+                Series chartSeries = new Series(this.dataSource.Columns[i].ColumnName);
+
+                chartSeries.XValueMember = "실무";
+                chartSeries.YValueMembers = this.dataSource.Columns[i].ColumnName;
+                chartSeries.ChartType = SeriesChartType.Radar;
+
+                chartSeries.SetCustomProperty("RadarDrawingStyle", "Line");
+
+                chartSeries.MarkerStyle = MarkerStyle.Square;
+                chartSeries.MarkerSize = 15;
+                chartSeries.BorderWidth = 2;
+
+                this.chartControl.Series.Add(chartSeries);
+            }
+
+            ChartArea chArea = this.chartControl.ChartAreas[0];
+
+            chArea.AxisY.MajorGrid.Enabled = false;
+            chArea.AxisY.MajorTickMark.Enabled = false;
+
+            chArea.BackColor = Color.Transparent;
+
+            Legend chLegend = this.chartControl.Legends[0];
+
+            chLegend.BorderColor = Color.DarkGray;
+            chLegend.BorderWidth = 1;
+            chLegend.BackColor = Color.Transparent;
+            chLegend.Alignment = StringAlignment.Center;
+            chLegend.Docking = Docking.Bottom;
+
+        }
+    }
+}
